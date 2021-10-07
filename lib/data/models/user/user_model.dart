9d0ddc613013eaabe34bf3e012737dc05b4448f9
sniren210 <sniren2002@gmail.com>
2021-10-08 02:06:28 +0700

    add build runner

--- lib/data/models/user/user_model.dart
+++ lib/data/models/user/user_model.dart
@@ -211,3 +211,19 @@ class Wallet {
         "updated_at": updatedAt != null ? updatedAt!.toIso8601String() : null,
       };
 }
+
+// import 'package:json_annotation/json_annotation.dart';
+// part 'user.g.dart';
+
+// @JsonSerializable()
+// class User {
+//   String name, lastName, add;
+//   bool subscription;
+
+//   User({this.name,this.lastName,this.add,this.subscription,});
+
+//   factory User.fromJson(Map<String,dynamic> data) => _$UserFromJson(data);
+
+//   Map<String,dynamic> toJson() => _$UserToJson(this);
+
+// }
