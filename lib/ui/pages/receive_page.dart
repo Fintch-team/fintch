a216de28f8da18aa7bb8009ecdf56f9087833086
adithyafirmansyahputra <aditputrafirmansyah@gmail.com>
2021-10-10 20:21:57 +0700

    fix receive page

--- lib/ui/pages/receive_page.dart
+++ lib/ui/pages/receive_page.dart
@@ -3,9 +3,6 @@ import 'package:fintch/gen_export.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pretty_qr_code/pretty_qr_code.dart';
-import 'package:provider/src/provider.dart';
-
-import 'package:loader_overlay/loader_overlay.dart';
 
 class ReceivePage extends StatefulWidget {
   const ReceivePage({Key? key}) : super(key: key);
@@ -23,115 +20,117 @@ class _ReceivePageState extends State<ReceivePage> {
 
   @override
   Widget build(BuildContext context) {
-    return LoadingOverlay(
-      child: BlocConsumer<ReceiveBloc, HomeState>(
-        listener: (context, state) {
-          if (state is HomeLoading) {
-            context.loaderOverlay.show();
-          } else if (state is HomeFailure) {
-            context.loaderOverlay.hide();
-            Helper.snackBar(context, message: state.message, isFailure: true);
-          } else if (state is HomeSuccess) {
-            context.loaderOverlay.hide();
-          }
-        },
-        builder: (context, state) {
-          if (state is HomeSuccess) {
-            return Scaffold(
-              body: Background(
-                child: SafeArea(
-                  child: Container(
-                    padding: EdgeInsets.all(Helper.normalPadding),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        CustomAppBar(
-                          title: 'QR Code Saya',
-                        ),
-                        SizedBox(height: 20),
-                        _userCard(context, state.entity),
-                      ],
-                    ),
-                  ),
+    return Scaffold(
+      body: Background(
+        child: SafeArea(
+          child: Container(
+            padding: EdgeInsets.all(Helper.normalPadding),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                CustomAppBar(
+                  title: 'QR Code Saya',
                 ),
-              ),
-            );
-          }
-          return Center(child: CircularProgressIndicator());
-        },
+                SizedBox(height: 20),
+                _userCard(context),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
 
-  Expanded _userCard(BuildContext context, UserEntity user) {
+  Expanded _userCard(BuildContext context) {
     return Expanded(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Container(
-            height: MediaQuery.of(context).size.height * 0.6,
-            child: Stack(
-              clipBehavior: Clip.none,
-              alignment: Alignment.center,
-              children: [
-                Positioned.fill(
-                  child: Container(
-                    decoration: BoxDecoration(
-                      boxShadow: Helper.getShadowBold(),
-                      borderRadius: BorderRadius.circular(32),
-                      color: AppTheme.scaffold,
-                    ),
-                    padding: EdgeInsets.all(20),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
+          SizedBox(height: MediaQuery.of(context).size.height * 0.06),
+          Expanded(
+            child: Container(
+              child: BlocConsumer<ReceiveBloc, HomeState>(
+                listener: (context, state) {
+                  if (state is HomeFailure) {
+                    Helper.snackBar(context, message: state.message, isFailure: true);
+                  }
+                },
+                builder: (context, state) {
+                  if (state is HomeSuccess) {
+                    return Stack(
+                      clipBehavior: Clip.none,
+                      alignment: Alignment.center,
                       children: [
-                        SizedBox(
-                          height: MediaQuery.of(context).size.width * 0.12,
+                        Positioned.fill(
+                          child: Container(
+                            decoration: BoxDecoration(
+                              boxShadow: Helper.getShadowBold(),
+                              borderRadius: BorderRadius.circular(32),
+                              color: AppTheme.scaffold,
+                            ),
+                            padding: EdgeInsets.all(20),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                SizedBox(
+                                  height: MediaQuery.of(context).size.width * 0.12,
+                                ),
+                                Text(state.entity.name, style: AppTheme.headline3),
+                                SizedBox(height: 8),
+                                Text(state.entity.school.name, style: AppTheme.text3),
+                                SizedBox(height: 8),
+                                Text(state.entity.id.toString(), style: AppTheme.text3.purple),
+                                SizedBox(height: Helper.normalPadding),
+                                Expanded(
+                                  child: Center(
+                                    child: PrettyQr(
+                                      image: AssetImage(Resources.icFintchPointPng),
+                                      size: MediaQuery.of(context).size.height * 0.3,
+                                      data: state.entity.id.toString(),
+                                      errorCorrectLevel: QrErrorCorrectLevel.M,
+                                      roundEdges: true,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
                         ),
-                        Text(user.name, style: AppTheme.headline3),
-                        SizedBox(height: 8),
-                        Text(user.school.name, style: AppTheme.text3),
-                        SizedBox(height: 8),
-                        Text(user.id.toString(), style: AppTheme.text3.purple),
-                        SizedBox(height: Helper.normalPadding),
-                        Expanded(
-                          child: Center(
-                            child: PrettyQr(
-                              image: AssetImage(Resources.icFintchPointPng),
-                              size: MediaQuery.of(context).size.height * 0.3,
-                              data: user.id.toString(),
-                              errorCorrectLevel: QrErrorCorrectLevel.M,
-                              roundEdges: true,
+                        Positioned(
+                          top: -MediaQuery.of(context).size.width * 0.12,
+                          child: Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(64),
+                              boxShadow: Helper.getShadow(),
+                            ),
+                            child: CustomNetworkImage(
+                              imgUrl: state.entity.img,
+                              borderRadius: 64,
+                              width: MediaQuery.of(context).size.width * 0.24,
+                              height: MediaQuery.of(context).size.width * 0.24,
                             ),
                           ),
                         ),
                       ],
-                    ),
-                  ),
-                ),
-                Positioned(
-                  top: -MediaQuery.of(context).size.width * 0.12,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(64),
-                      boxShadow: Helper.getShadow(),
-                    ),
-                    child: CustomNetworkImage(
-                      imgUrl: Dummy.profileImg,
-                      borderRadius: 64,
-                      width: MediaQuery.of(context).size.width * 0.24,
-                      height: MediaQuery.of(context).size.width * 0.24,
-                    ),
-                  ),
-                ),
-              ],
+                    );
+                  }
+                  return Center(child: CircularProgressIndicator());
+                },
+              ),
             ),
           ),
           SizedBox(height: 40),
           CustomButton(
             text: 'Scan QR Code',
-            onTap: () {},
+            onTap: () => Navigator.of(context).pushNamed(PagePath.pay),
+            isUpper: false,
+          ),
+          SizedBox(height: Helper.normalPadding),
+          CustomButton(
+            text: 'Top Up',
+            onTap: () => Navigator.of(context).pushNamed(PagePath.topUp),
+            isUpper: false,
           ),
         ],
       ),
